daa043bb9bb124aadd2fc5c92b1247f39cba337c
valentina-kustikova <valentina.kustikova@gmail.com>
2015-10-06 14:32:30 +0300

    Matlab format for operations.

--- scripts/prepare_aug.m
+++ scripts/prepare_aug.m
@@ -1,61 +1,67 @@
-#Script for segmentation data extraction
+% Script for conversion of the Semantic Boundaries Dataset
+% http://www.cs.berkeley.edu/~bharath2/codes/SBD/download.html
+% to PASCAL VOC format
 
-dataset_dir = "C:/Programming/CProjects/nnsu/datasets/benchmark_RELEASE/dataset";
-dataset_info_dir = "/inst";
-dataset_list_name = [dataset_dir, "/val.txt"];
+% This script should be used for training DeepLab models
+% on augmented PASCAL VOC 2012 dataset
+function [] = SBDataset2VOC(dataset_dir)
+dataset_info_dir = '/inst';
+dataset_list_name = [dataset_dir, '/val.txt'];
 
-  output_path = "/SegmentationValAug_inst";
-dataset_info_name = [dataset_dir, "/val_aug_inst.txt"];
+output_path = '/SegmentationValAug_inst';
+dataset_info_name = [dataset_dir, '/val_aug_inst.txt'];
 
 
 
 
-dataset_list_fid = fopen(dataset_list_name, "r");
+dataset_list_fid = fopen(dataset_list_name, 'r');
 
 if (dataset_list_fid == -1)
-  printf("Error: Failed to load a file '%s'. Aborting.\n", dataset_list_name);
+  printf('Error: Failed to load a file %s. Aborting.\n', dataset_list_name);
   exit;
 endif
 
-dataset_list = textscan(dataset_list_fid, "%s");
+dataset_list = textscan(dataset_list_fid, '%s');
 
 fclose(dataset_list_fid);
 
 dataset_list_length = length(dataset_list{1});
-printf("Entries count: %d\n", dataset_list_length);
+printf('Entries count: %d\n', dataset_list_length);
 
-dataset_info = fopen(dataset_info_name, "w+");
+dataset_info = fopen(dataset_info_name, 'w+');
 if (dataset_info_name == -1)
-  printf("Error: Failed to create a file '%s'. Aborting.\n", dataset_info_name);
+  printf('Error: Failed to create a file %s. Aborting.\n', dataset_info_name);
   exit;
-endif
+end
 
 output_dir = [dataset_dir, output_path];
 
 for i = 1: dataset_list_length
   sample_name = dataset_list{1}{i};
-  img_info_name = [dataset_dir, dataset_info_dir, "/", sample_name, ".mat"];
+  img_info_name = [dataset_dir, dataset_info_dir, '/', sample_name, '.mat'];
   try 
     load(img_info_name);
-  catch
-    printf("Error: File '%s' not found. Skipping.\n", img_info_name);
+  catch exception
+    printf(getReport(exception));
+    printf('Error: File %s not found. Skipping.\n', img_info_name);
     continue;
-  end_try_catch
+  end
   
   img = GTinst.Segmentation;
     
   try
-    out_img_name = [output_dir, "/", sample_name, ".png"];
+    out_img_name = [output_dir, '/', sample_name, '.png'];
     imwrite(img, out_img_name);
-    #printf("Image created successfully: '%s'.\n", out_img_name);
-  catch
-    printf("Error: Failed to write image '%s'\n", out_img_name);
-  end_try_catch
+    %printf("Image created successfully: '%s'.\n", out_img_name);
+  catch exception
+    printf(getReport(exception));
+    printf('Error: Failed to write image %s\n', out_img_name);
+  end
   
-  out_name = [output_path, "/", sample_name];   
-  fprintf(dataset_info, "%s %s\n", sample_name, out_name);
-endfor
+  out_name = [output_path, '/', sample_name];   
+  fprintf(dataset_info, '%s %s\n', sample_name, out_name);
+end
 
 fclose(dataset_info);
 
-printf("Operation completed successfully!\n");
+printf('Operation completed successfully!\n');
