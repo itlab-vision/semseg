774f40c7e6e01f0b6bc48d8bd293e0ac04d87739
valentina-kustikova <valentina.kustikova@gmail.com>
2015-10-12 14:36:29 +0300

    Eliminate circular contour duplication.

--- scripts/drawContours.m
+++ scripts/drawContours.m
@@ -12,6 +12,12 @@ function [img_r, img_g, img_b] = drawContours(img_r, img_g, img_b, ...
 % 5 x 5 pixels for contour
 shifts = [-2 -1 0 1 2];
 
+% add border to eliminate circular contour duplication
+bnd_logical = padarray(bnd_logical, [2 2]);
+img_r = padarray(img_r, [2 2]);
+img_g = padarray(img_g, [2 2]);
+img_b = padarray(img_b, [2 2]);
+
 for i = 1 : length(shifts)
     for j = 1 : length(shifts)
         shift = [shifts(i), shifts(j)];
@@ -22,3 +28,8 @@ for i = 1 : length(shifts)
         img_b(bnd_logical_shift) = color_contour(3);
     end
 end
+
+% remove border
+img_r = img_r(3 : size(img_r, 1) - 2, 3 : size(img_r, 2) - 2);
+img_g = img_g(3 : size(img_g, 1) - 2, 3 : size(img_g, 2) - 2);
+img_b = img_b(3 : size(img_b, 1) - 2, 3 : size(img_b, 2) - 2);
